bf130d42c63f7977a201dac86bba2c941de6bb85
chillsterz3434 <coopdedoop5668@gmail.com>
2023-02-28 11:38:40 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-
 import React from "react";
 
 function App() {
@@ -16,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {!data ? "Loading..." : data}
         </p>
--- server/index.js
+++ server/index.js
@@ -10,31 +10,26 @@ const app = express();
 
 const { PythonShell } = require("python-shell");
 
-// let options = {
-//   scriptPath: "C:/Users/BigMike/Documents/adSpring2023/SeniorProject/DaViToMo_server/server",
-//   args: ["Morty_Smith"]
-// };
 
-// PythonShell.run("download.py", options, (err, res) => {
-//     if (err) console.log(err);
-//     if (res) console.log(res);
-// })
 
 app.get("/api", (req, res) => {
-    var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python', ['topic.py']);
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    // in cloe even we are sure that stream from childl dprocess is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.send(dataToSend)
-    });
+
+    res.json({ message: "Hello from server!" })
+
+    // var dataToSend;
+    // // spawn new child process to call the python script
+    // const python = spawn('python', ['topic.py']);
+    // // collect data from script
+    // python.stdout.on('data', function (data) {
+    //     console.log('Pipe data from python script ...');
+    //     dataToSend = data.toString();
+    // });
+    // // in cloe even we are sure that stream from childl dprocess is closed
+    // python.on('close', (code) => {
+    //     console.log(`child process close all stdio with code ${code}`);
+    //     // send data to browser
+    //     res.send(dataToSend)
+    // });
     
 });
 
