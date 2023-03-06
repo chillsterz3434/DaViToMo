3aeeebbf8a4c47936f98da46dd90cdd19d86be22
chillsterz3434 <coopdedoop5668@gmail.com>
2023-03-06 00:38:01 -0500

    Script runs on client now :)

--- server/index.js
+++ server/index.js
@@ -1,35 +1,35 @@
 // server/index.js
 
 const express = require("express");
-// const {spawn} = require('child_process');
+const {spawn} = require('child_process');
 
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// const { PythonShell } = require("python-shell");
+const { PythonShell } = require("python-shell");
 
 
 
 app.get('/api', (req, res) => {
 
-    res.json({ message: "Hello from server!" });
+    // res.json({ message: "Hello from server!" });
 
-    // var dataToSend;
-    // // spawn new child process to call the python script
-    // const python = spawn('python', ['topic.py']);
-    // // collect data from script
-    // python.stdout.on('data', function (data) {
-    //     console.log('Pipe data from python script ...');
-    //     dataToSend = data.toString();
-    // });
-    // // in cloe even we are sure that stream from childl dprocess is closed
-    // python.on('close', (code) => {
-    //     console.log(`child process close all stdio with code ${code}`);
-    //     // send data to browser
-    //     res.send(dataToSend)
-    // });
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', ['topic.py']);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+    // in cloe even we are sure that stream from childl dprocess is closed
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        res.send({script: {dataToSend}})
+    });
 });
 
     
