94b47e85a5961c6a80b0aaf4826383e7072f2aed
chillsterz3434 <coopdedoop5668@gmail.com>
2023-03-13 13:02:13 -0400

    Trying to connect to mongodb

--- server/download.py
+++ server/download.py
@@ -1,37 +1,40 @@
-import os
 import re
 import wikipedia as wp
-import sys
+from pymongo import MongoClient
 
-# this is the name of the Wikipedia page
+# Set up the MongoDB client and database
+client = pymongo.MongoClient("mongodb+srv://mgcooper5668:<password>@cluster0.0xcpymn.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+db = client["DaViToMo"]
+
+# Create a new collection to store the articles
+collection = db["Articles"]
+
+# This is the name of the Wikipedia page
 main_page = "Morty_Smith"  # from the TV show "Rick & Morty"
-#main_page = "Turing_Award"
-#main_page = "The_Matrix"
-# main_page = sys.argv[1] # getting arguments from index.js
+# main_page = "Turing_Award"
+# main_page = "The_Matrix"
 
-# fetch the webpage
-main = wp.page(main_page,auto_suggest=False)
-print("== Downloading %s: %d links" % (main_page,len(main.links)))
+# Fetch the webpage
+main = wp.page(main_page, auto_suggest=False)
+print("== Downloading %s: %d links" % (main_page, len(main.links)))
 
-# print("Downloading to 'data' folder")
-# print("Remove or rename 'data' folder to download new dataset")
-# print("Type Y and Enter to continue")
-# yes = input().lower()
-# if yes != "y": exit()
+print("Downloading to MongoDB database")
+print("Type Y and Enter to continue")
+yes = input().lower()
+if yes != "y": exit()
 
-# make a data/ directory, if it doesn't exist
-os.makedirs("data", exist_ok=True)
-
-# add page, and all links on page to the list of pages to download
+# Add page, and all links on page to the list of pages to download
 links = [main_page] + main.links
 for link in links:
-    # skip pages that are "List of" or "Category" pages
+    # Skip pages that are "List of" or "Category" pages
     if link.startswith("List"): continue
 
-    # try to download the page
+    # Try to download the page
     print(link)
     try:
-        page = wp.page(link,auto_suggest=False,preload=False)
+        page = wp.page(link, auto_suggest=False, preload=False)
     except wp.exceptions.PageError:
         print("    page not found, skipping")
         continue
@@ -42,21 +45,18 @@ for link in links:
         print("    unexpected error, skipping")
         continue
 
-    # check if we already downloaded the page, and skip if it exists
+    # Check if we already downloaded the page, and skip if it exists
     pageid = page.pageid
-    filename = "data/%s.txt" % pageid
-    if os.path.exists(filename):
+    if collection.find_one({"id": pageid}):
         print("    page previously saved, skipping")
         continue
 
-    # get the title and text from the page
+    # Get the title and text from the page
     title = page.title
     text = page.content
-    # remove non-alphabetic characters from text
+    # Remove non-alphabetic characters from text
     clean_text = re.sub('[^A-Za-z]+', ' ', text)
 
-    # save article as text file
-    with open(filename,'w') as f:
-        f.write("title: %s\n" % title)
-        f.write("id: %s\n" % pageid)
-        f.write(clean_text)
+    # Insert the article into the MongoDB collection
+    article = {"title": title, "id": pageid, "text": clean_text}
+    collection.insert_one(article)
