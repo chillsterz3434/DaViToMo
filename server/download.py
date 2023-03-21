6c882146e9861b703dc3f24e1508839263625f5d
chillsterz3434 <coopdedoop5668@gmail.com>
2023-03-21 11:05:57 -0400

    copying diff from main

--- server/download.py
+++ server/download.py
@@ -1,62 +1,69 @@
-import os
 import re
 import wikipedia as wp
-import sys
+from pymongo import MongoClient
 
-# this is the name of the Wikipedia page
-main_page = "Morty_Smith"  # from the TV show "Rick & Morty"
-#main_page = "Turing_Award"
-#main_page = "The_Matrix"
-# main_page = sys.argv[1] # getting arguments from index.js
+# # Set up the MongoDB client and database INSERT PERSONAL UNAME AND PWORD
+client = MongoClient("mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.0xcpymn.mongodb.net/?retryWrites=true&w=majority")
 
-# fetch the webpage
-main = wp.page(main_page,auto_suggest=False)
-print("== Downloading %s: %d links" % (main_page,len(main.links)))
+db = client["DaViToMo"]
 
-# print("Downloading to 'data' folder")
-# print("Remove or rename 'data' folder to download new dataset")
-# print("Type Y and Enter to continue")
-# yes = input().lower()
-# if yes != "y": exit()
 
-# make a data/ directory, if it doesn't exist
-os.makedirs("data", exist_ok=True)
 
-# add page, and all links on page to the list of pages to download
-links = [main_page] + main.links
-for link in links:
-    # skip pages that are "List of" or "Category" pages
-    if link.startswith("List"): continue
+# This is the name of the Wikipedia page
+# main_page = "Morty_Smith"  # from the TV show "Rick & Morty"
+# main_page = "Turing_Award"
+# main_page = "The_Matrix"
 
-    # try to download the page
-    print(link)
-    try:
-        page = wp.page(link,auto_suggest=False,preload=False)
-    except wp.exceptions.PageError:
-        print("    page not found, skipping")
-        continue
-    except wp.exceptions.DisambiguationError:
-        print("    ambiguous name, skipping")
-        continue
-    except:
-        print("    unexpected error, skipping")
-        continue
+main_page = input("Enter an article with a _ between each word: ")
 
-    # check if we already downloaded the page, and skip if it exists
-    pageid = page.pageid
-    filename = "data/%s.txt" % pageid
-    if os.path.exists(filename):
-        print("    page previously saved, skipping")
-        continue
+# # Create a new collection to store the articles with the name of the main page
+collection = db[main_page]
 
-    # get the title and text from the page
-    title = page.title
-    text = page.content
-    # remove non-alphabetic characters from text
-    clean_text = re.sub('[^A-Za-z]+', ' ', text)
+col = collection.find()
+if col == 0:
+    # Fetch the webpage
+    main = wp.page(main_page, auto_suggest=False)
+    print("== Downloading %s: %d links" % (main_page, len(main.links)))
 
-    # save article as text file
-    with open(filename,'w') as f:
-        f.write("title: %s\n" % title)
-        f.write("id: %s\n" % pageid)
-        f.write(clean_text)
+    print("Downloading to MongoDB database")
+    print("Type Y and Enter to continue")
+    yes = input().lower()
+    if yes != "y": exit()
+
+    # Add page, and all links on page to the list of pages to download
+    links = [main_page] + main.links
+    for link in links:
+        # Skip pages that are "List of" or "Category" pages
+        if link.startswith("List"): continue
+
+        # Try to download the page
+        print(link)
+        try:
+            page = wp.page(link, auto_suggest=False, preload=False)
+        except wp.exceptions.PageError:
+            print("    page not found, skipping")
+            continue
+        except wp.exceptions.DisambiguationError:
+            print("    ambiguous name, skipping")
+            continue
+        except:
+            print("    unexpected error, skipping")
+            continue
+
+        # Check if we already downloaded the page, and skip if it exists
+        pageid = page.pageid
+        if collection.find_one({"id": pageid}):
+            print("    page previously saved, skipping")
+            continue
+
+        # Get the title and text from the page
+        title = page.title
+        text = page.content
+        # Remove non-alphabetic characters from text
+        clean_text = re.sub('[^A-Za-z]+', ' ', text)
+
+        # Insert the article into the MongoDB collection
+        article = {"title": title, "id": pageid, "text": clean_text}
+        collection.insert_one(article)
+    
+
