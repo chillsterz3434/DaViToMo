dd272572992abc93c70a4f9ab7997823831bc8ce
chillsterz3434 <coopdedoop5668@gmail.com>
2023-03-22 01:14:54 -0400

    Working on article submission

--- client/src/ArticleSearch/ArticleSearch.js
+++ client/src/ArticleSearch/ArticleSearch.js
@@ -1,20 +1,95 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 import "./ArticleSearch.css";
 
+//TODO: Still need to figure out how to get article into the GET request
 
 function ArticleSearch() {
+
+  const [searchInput, setSearchInput] = useState("");
+  const [article, setArticle] = useState("");
+  const [data, setData] = useState("");
+  const [url, setUrl] = useState("");
+
+
+
+  const handleClick = () => {
+    setArticle(searchInput);
+    setUrl('/api/'+article);
+    // submitArticle({title: article})
+  }
+
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.script.dataToSend))
+  // })
+  
+  // const form = document.querySelector('form')
+  // if(form){
+  //   form.addEventListener('submit', (e) => {
+  //     e.preventDefault();
+  //     submitArticle(article)
+  //   }, []);
+  // }
+
+  function filterData(event){
+    setSearchInput(event.target.value)
+  }
+
+  React.useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => setData(data.script.dataToSend))
+  }, [])
+
+  // async function submitArticle(a) {
+  //   try {
+  //     const response = await fetch('/api/', {
+  //       method: "GET",
+        
+  //     })
+  //     .then((res) => res.json())
+  //     .then((data) = setData(data.script.dataToSend))
+  //     if (!response.ok) {
+  //       throw new Error('Request failed with status '+response.status)
+  //     }
+  //     // const data = await response.json()
+      
+  //     // console.log('Article submitted: '+data)
+  //     // console.log(response.body)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  
+
+  
+
 
 
 
     return (
         
-  <body>
+  <div>
 
     <main>
-      <form>
-        <input type="text" id="search-input" placeholder="Enter article..." />
-        <button type="submit" id="search-btn">Run Script</button>
-      </form>
+      
+        <input type="text" name="article" placeholder="Enter article..."  onChange={filterData} value={searchInput}/>
+        <button type="submit" id="search-btn" onClick={handleClick}>Run Script</button>
+        
+
+      <div>
+        <header>
+          <p>{!data ? "Loading..." : data}</p>
+          <p>{searchInput}</p>
+          <p>{article}</p>
+        </header>
+        <form>
+
+        </form>
+      </div>
+      
     </main>
 
     <footer>
@@ -22,7 +97,7 @@ function ArticleSearch() {
     </footer>
 
     <script src="app.js"></script>
-  </body>
+  </div>
     )
 }
 export default ArticleSearch;
--- server/topic_run.js
+++ server/topic_run.js
@@ -1,24 +1,47 @@
-// server/index.js
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const {spawn} = require('child_process');
-
-
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+const cors = require('cors');
 
 const { PythonShell } = require("python-shell");
 
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
 
 
-app.get('/api', (req, res) => {
 
-    // res.json({ message: "Hello from server!" });
 
+
+
+// app.get("/api", (req, res) => {
+//     res.json({title: article})
+// })
+
+// // Route that receives a POST request to /api
+// app.post('/api', function (req, res) {
+//   const body = req.query
+//   res.set('Content-Type', 'text/plain')
+//   console.log(body)
+// //   setArticle(article)
+//   res.send(body)
+  
+// })
+
+
+
+
+
+
+
+app.get('/api/:title', (req, res) => {
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python', ['topic.py', 'Morty_Smith']);
+    const python = spawn('python', ['topic.py', req.params.title]);
     // collect data from script
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
@@ -29,9 +52,7 @@ app.get('/api', (req, res) => {
         console.log(`child process close all stdio with code ${code}`);
         // send data to browser
         res.send({script: {dataToSend}})
-        res.end();
     });
-    
     
 });
 
@@ -40,3 +61,4 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
