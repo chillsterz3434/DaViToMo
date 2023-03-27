186174c3b95c10cfe82c8c3e2c8543ce359f099e
chillsterz3434 <coopdedoop5668@gmail.com>
2023-03-27 15:50:12 -0400

    Completed topic card component

--- client/src/Cards/TopicCard.js
+++ client/src/Cards/TopicCard.js
@@ -1,62 +1,37 @@
 import React, {useState, useEffect} from 'react';
-// import PropTypes from 'prop-types'
-// import Card from './Card';
+import PropTypes from 'prop-types'
+import Card from './Card';
 import './TopicCard.css'
 
 
 
 const TopicCard = (props) => {
 
-    const [topthree, setTopthree] = useState([]);
-//     return(
-//         <Card key={props.topic.id} className='topic-card'>
-//             <div className="card-content">
-//                 <div className='topic-title'>{props.topic.title}</div>
-//             </div>
-//         </Card>
-//     );
-// };
-
-// TopicCard.propTypes = {
-//     topic: PropTypes.shape({
-//         title: PropTypes.string
-//     }),
-
-// function getTopThree(words) {
-//     for(let i=0; i<3; i++){
-//         topthree[i] = words[i];
-//     }
-//     setTopthree(topthree)
-// }
-
-// useEffect(() => {
-//     getTopThree(props.words);
-// })
-
-return(
-    <>
-    <button className='card'>
+    return(
+        <Card key={props.topic.title} className='topic-card'>
             <div className='card-content'>
-                <div className='card-header'>
-                    <div className='topic-title'>{props.title}</div>
-                </div>
-                <div className='card-firstthree'>
+                 <div className='card-header'>
+                     <div className='topic-title'>{props.title}</div>
+                 </div>
+                 <div className='card-firstthree'>
                     
-                {props.words.slice(0,3)?.map(word => (
-                    <div className='word' key={word}>
-                        {word}
-                        </div>
-                ))}
-            </div>
-            </div>
-    </button>
-        
-    </>
-)
+                 {props.words.slice(0,3)?.map(word => (
+                     <div className='word' key={word}>
+                         {word}
+                         </div>
+                 ))}
+             </div>
+             </div>
+        </Card>
+    );
+};
 
+TopicCard.propTypes = {
+    topic: PropTypes.shape({
+        title: PropTypes.string,
+        words: PropTypes.arrayOf(PropTypes.string),
+    }),
 }
-
-
 
 
 
